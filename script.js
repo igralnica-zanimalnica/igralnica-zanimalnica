0f8db3e929fc1a5b7b9796b722850b3ad983e16b
Pavel Bogdanov <1339849+pavelnaut@users.noreply.github.com>
2023-07-25 18:24:37 +0300

    Add contacts content (#4)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Update the current year
+// Update the current year for the footer
 document.addEventListener('DOMContentLoaded', function() {
     const footerHTML = `<p>&copy; ${new Date().getFullYear()} Игралница-Занималница. Всички права запазени.</p>`;
     const footer = document.getElementsByTagName('footer')[0];
