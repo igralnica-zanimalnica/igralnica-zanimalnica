f7d8802579b4505c2dc8dabcac2f24d2e790a349
Pavel Bogdanov <1339849+pavelnaut@users.noreply.github.com>
2023-07-27 12:58:53 +0300

    Add services content (#15)

--- script.js
+++ script.js
@@ -15,7 +15,32 @@ document.addEventListener('DOMContentLoaded', function() {
     Важно за нас е родителите да ни имат доверие, да се чувстват спокойни, улеснени и сигурни, че детето им ще е щастливо, подготвено и устремено.</p>`
 
     const servicesHTML = 
-    `<h1>Услуги</h1>`
+    `<h1>Услуги</h1><p>
+    <p>
+    Полудневно посещение<br>
+    <ul>
+      <li>Месечна такса – 300 лв./20 работни дни / Не се възстановява при отсъствие на детето</li>
+      <li>Седмична такса – 80лв./16лв.на ден</li>
+      <li>Дневна такса – 20 лв.</li>
+      <li>Удължено работно време – 5 лв./час за всеки започнат час.</li>
+      <li>Почасово – 6 лв./час</li>
+      <li>Топъл обяд - 6 лв. на ден /заплаща се в началото на всяка седмица/</li>
+    </ul>
+
+    Целодневно посещение: 20лв./ден<br>
+    Топъл обяд: 6лв.<br>
+    Допълнителни занимания:<br>
+
+    <ul>
+      <li>Арт студио "Игралница занималница", рисуване и приложни изкуства - 16лв. / 2 часа, всяка събота от 10:00-12:00 или 12:00-14:00/</li>
+      <li>Индивидуални уроци по български език и математика - 20лв./час</li>
+      <li>Индивидуални уроци по английски език-25лв./час</li>
+    </ul>
+    
+    <br><br>
+    *Месечната такса се заплаща от 1-5 число на текущия месец, включва полудневна занималня, взимане и водене на детето от и до училище, не се възстановява при отсъствие<br>
+    
+    *Седмичната такса се заплаща в началото на всяка седмица, включва полудневна занималня, взимане и водене на детето от и до училище.</p>`
 
     const contactsHTML = 
     `<h1>Контакти</h1><p>Служебен телефон  0889 656789 <br><br>
@@ -59,14 +84,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('nav a');
     const content = document.querySelector('main');
 
+    function updateTitle(pathname) {
+        // Document title is the navbar text + company name
+        let navbarContent = document.querySelector(`a[href="${pathname}"]`).textContent;
+        document.title = navbarContent + " | Игралница-Занималница"
+    }
+
     function navigateTo(url) {
         let urlObject = new URL(url);
         let path = urlObject.pathname;
         // Change the HTML of the main content
         content.innerHTML = routes[path];
-        // Document title is the navbar text + company name
-        let navbarContent = document.querySelector(`a[href="${path}"]`).textContent;
-        document.title = navbarContent + " | Игралница-Занималница"
+        updateTitle(path);
         // Modify browser's history and change the URL without triggering a full page reload
         window.history.pushState({}, '', path);
     }
