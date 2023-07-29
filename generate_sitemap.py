1f614335da9a2f56e98806199407f568aa557e57
Pavel Bogdanov <1339849+pavelnaut@users.noreply.github.com>
2023-07-29 13:28:03 +0300

    Fix image dimensions (#19)
    * Add comments
    
    * Fix image dimensions

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -10,12 +10,13 @@ base_url = "https://igralnica-zanimalnica.netlify.app"
 date_pattern = r'\d{4}-\d{2}-\d{2}'
 last_date = datetime.strptime("2023-07-25", "%Y-%m-%d")
 
+# Execute git blame and get the output
 cmd = ["git", "blame", "script.js"]
 result = subprocess.run(cmd, capture_output=True, text=True, encoding='utf-8', errors='ignore')
 blame = result.stdout
 blame = blame.replace('`\n', '`endofconst\n')
 
-
+# Get "routes" variable
 pattern = r"const routes = {[\s\S]*?};"
 match = re.search(pattern, blame)
 if match:
@@ -26,18 +27,22 @@ if match:
     for key, value in extracted_dict:
         urls[value] = key
 
-
+# Sitemap header
 sitemap = """<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n"""
 
 for line in blame.split('\n'):
+    # Get the url for the <loc> tag
     if 'const' in line and 'HTML' in line:
         loc = line.split('const ')[1].replace(' ', '').replace('=', '')
         sitemap += f"""<url>\n\t<loc>{base_url}{urls[loc]}</loc>\n"""
+    # Get the date from git blame
     date = re.findall(date_pattern, line)
     if not date:
         continue
     date = datetime.strptime(date[0], "%Y-%m-%d")
+    # Compare date in current line with latest change we have
     if date > last_date:
+        # We set this as our <lastmod> until more recent date pops up for the <loc>
         last_date = date
     if "`endofconst" in line:
         sitemap += f"""\t<lastmod>{last_date.strftime("%Y-%m-%d")}</lastmod>\n</url>\n"""
@@ -45,5 +50,6 @@ for line in blame.split('\n'):
     if '// End of HTML' in line:
         break
 
+# Save string into file
 with open("sitemap.xml", "w") as file:
     file.write(sitemap)
