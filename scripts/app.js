933674a5e68aa15b78566df6a928885b7ede3f7e
pavelnaut <pavelmbogdanov@gmail.com>
2023-09-13 15:01:06 +0300

    Add other images

--- scripts/app.js
+++ scripts/app.js
@@ -29,19 +29,42 @@ document.addEventListener('DOMContentLoaded', function() {
       return'rgb(' + (rgbColor['r'] + brightness) + ',' + (rgbColor['g'] + brightness) + ',' + (rgbColor['b'] + brightness) + ')'
     };
 
+    const hamburger = 
+    `
+    <svg xmlns="http://www.w3.org/2000/svg" alt="Hamburger menu button" id="hamburgerIcon" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>`
+    
+    const chevronDown = 
+    `<svg xmlns="http://www.w3.org/2000/svg"  alt="Chevron expand button" id="chevronDownIcon" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>`
+    
     const forSignUpAndInformation = `<p>Записване и информация - 0889656789, 0887779783 или на място - ж.к.Чайка, бл. 189, от понеделник до петък, от 8:00 до 18:00.</p>`
 
     // Start of HTML
     const homeHTML = 
-    `<p>Детски център "Игралница Занималница", предлага училищна занималня за ученици от 1 до 5 клас, както и за деца от подготвителните групи към училищата.</p>
+    `
+    <div class="contentContainer">
+      <img class="contentImage framed fixed" src="images/igralnica_inside.jpg">
+      <div class="textContainer fixed">
+        <p>Детски център "Игралница Занималница", предлага училищна занималня за ученици от 1 до 5 клас, както и за деца от подготвителните групи към училищата.</p> 
+      </div>
+    </div>
 
-    <p>Преподавателите ни са специалисти с дългогодишен опит в областта на хуманитарните и точните науки, изкуствата и музиката.</p>
+    <div class="contentContainer reverseFlex">
+      <div class="textContainer">
+        <p>Преподавателите ни са специалисти с дългогодишен опит в областта на хуманитарните и точните науки, изкуствата и музиката.</p>
+      </div>
+      <img class="contentImage framed" src="images/igralnica_entrance.jpg">
+    </div>
+
+  <div class="contentContainer">
+    <img class="contentImage framed" src="images/table.jpg">
+    <div class="textContainer">
+      <p>Вярваме, че мечтите създават бъдеще. Насърчаваме децата да бъдат радостни без официален повод и ги подкрепяме в стремежа им винаги да са заети с нещо.</p>
+    </div>
+  </div>
+
+
     
-    <p>Вярваме, че мечтите създават бъдеще. Насърчаваме децата да бъдат радостни без официален повод и ги подкрепяме в стремежа им винаги да са заети с нещо.</p>
     
-    
-    <p><q>Работата е вдъхновение. Да сме навън е приключение. Книгата е удоволствие. Детето е човече с голяма мечта и личен талант, който уважаваме. Обичаме да четем. Правим го заедно и знаем, че буквите разкриват тайни. Призванието ни е да бъдем Учители.</q>
-     - Илиана Йорданова, основател на "Игралница Занималница"</p>
     <br>
     <p>В основната ни програма:
     <ul>
@@ -65,13 +88,21 @@ document.addEventListener('DOMContentLoaded', function() {
       <li>Уроци и курсове по математика</li>
       <li>Логопедична консултация и помощ</li>
     </ul></p>
+
+    <div class="contentContainer reverseFlex">
+      <div class="textContainer">
+        <p><q>Работата е вдъхновение. Да сме навън е приключение. Книгата е удоволствие. Детето е човече с голяма мечта и личен талант, който уважаваме. Обичаме да четем. Правим го заедно и знаем, че буквите разкриват тайни. Призванието ни е да бъдем Учители.</q>
+      - Илиана Йорданова, основател на "Игралница Занималница"</p></div>
+      <img class="contentImage framed" src="images/ilka.jpg">
+    </div>
+
     `
     const aboutHTML = 
     `<div style="display: flex;align-items: center;flex-direction: column;">
         <h3 style="font-size: 100%; white-space: nowrap; margin: 0vw 3vw; margin-top: 2vw;">Здравейте, ние сме Нина и Роси!</h3>
         <div style="margin: 2vw 0vw; display: flex;align-items: center;flex-wrap: wrap;justify-content: center;">
-          <img src="images/nina.jpg" title="Нина" class="profileImage" alt="Снимка на Нина" style="display: inline;">
-          <img src="images/rosi.jpg" title="Роси" class="profileImage" alt="Снимка на Роси" style="display: inline;">
+          <img src="images/nina.jpg" title="Нина" class="profileImage framed" alt="Снимка на Нина" style="display: inline;">
+          <img src="images/rosi.jpg" title="Роси" class="profileImage framed" alt="Снимка на Роси" style="display: inline;">
         </div>
         <div>
           <p>За нас е удоволствие да  работим и помагаме за решаването на безбройните въпроси при обучението и възпитанието на детето.</p>
@@ -100,8 +131,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     <p>Facebook - <a href="https://www.facebook.com/IgralnicaZanimalnica">Игралница Занималница</a></p>
     <p>кв. Чайка бл. 189 / до бл. 23 /</p>
-    <iframe class="googleMap" src="https://www.google.com/maps/embed?pb=!4v1694336757849!6m8!1m7!1ssvKDniCXQ2BeTGZgkuGLZQ!2m2!1d43.21386349649769!2d27.93854024952334!3f3.1267098522612784!4f-0.9822007946029316!5f1.9587109090973311" style="border:0;" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <iframe class="googleMap" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7990.307874775659!2d27.93378972147678!3d43.214117923401915!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a454401d8909c1%3A0xe407984e20a12751!2z0JjQs9GA0LDQu9C90LjRhtCwINCX0LDQvdC40LzQsNC70L3QuNGG0LA!5e0!3m2!1sbg!2sbg!4v1694365208587!5m2!1sbg!2sbg" style="border:0;" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+    <iframe class="googleMap framed" src="https://www.google.com/maps/embed?pb=!4v1694336757849!6m8!1m7!1ssvKDniCXQ2BeTGZgkuGLZQ!2m2!1d43.21386349649769!2d27.93854024952334!3f3.1267098522612784!4f-0.9822007946029316!5f1.9587109090973311" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe class="googleMap framed" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7990.307874775659!2d27.93378972147678!3d43.214117923401915!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a454401d8909c1%3A0xe407984e20a12751!2z0JjQs9GA0LDQu9C90LjRhtCwINCX0LDQvdC40LzQsNC70L3QuNGG0LA!5e0!3m2!1sbg!2sbg!4v1694365208587!5m2!1sbg!2sbg" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>`
     const schoolZanimalnicaHTML = 
     `<h1>Училищна Занималня</h1><p>От 15-ти Септември до 31-ви Май предламе училищна занималня за ученици от 1 до 5  клас, както и за деца от подготвителните групи към училищата.</p>
 
@@ -135,8 +166,8 @@ document.addEventListener('DOMContentLoaded', function() {
     <p>Допълнителни занимания:
     <ul>    
       <li>
-        Арт студио "Игралница Занималница", рисуване и приложни изкуства - 20лв./2 часа, с включени материали, всяка събота от 10:00-12:00
-        <br><input type="checkbox" id="hiddenToggle"><label for="hiddenToggle" class="arrow">Повече информация ▼</label>
+        Арт студио "Игралница Занималница", рисуване и приложни изкуства - 20лв./2 часа, с включени материали
+        <input type="checkbox" id="hiddenToggle"><label for="hiddenToggle" class="arrow">${chevronDown}</label>
         <div class="hiddenContent">
           <p>Рисувайки малките деца стават по-наблюдателни, чувствителни и възпитават у себе си усет към красивото и любов към природата.</p>
 
@@ -148,7 +179,7 @@ document.addEventListener('DOMContentLoaded', function() {
           
           <p>Тези, на които им се отдава скулптурата, правят релефи от пластилин, които отливат от гипс, а бъдещите графици рисуват с туш и перо и правят линогравюра.</p>
           
-          <p>Желаещите да кандидатстват след  4 и 7 клас в специализирано училище по изобразителни изкуства работят по изпитните задачи: натюрморт-етюд и фигурална композиция.</p>
+          <p>Желаещите да кандидатстват след 4 и 7 клас в специализирано училище по изобразителни изкуства работят по изпитните задачи: натюрморт-етюд и фигурална композиция.</p>
           
           <p>Рисува се върху картон с размер 35/50 см.,за да може рисунките да участват в различни конкурси.</p>
           
@@ -327,11 +358,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const mainContent = {
         '/': homeHTML,
-        '/за-нас': aboutHTML,
-        '/контакти': contactsHTML,
         '/училищна-занималня': schoolZanimalnicaHTML,
         '/лятна-игралница': summerIgralnicaHTML,
         '/лятна-занималница': summerZanimalnicaHTML,
+        '/за-нас': aboutHTML,
+        '/контакти': contactsHTML,
         '/admin': adminHTML,
         };
     
@@ -349,17 +380,13 @@ document.addEventListener('DOMContentLoaded', function() {
       `
       <nav class="navbar" id="desktopNavbar">
         <a href="/" class="navlink">Начало</a>
-        <a href="/за-нас" class="navlink">За Нас</a>
-        <a href="/контакти" class="navlink">Контакти</a>
         <a href="/училищна-занималня" class="navlink">Училищна Занималня</a>
         <a href="/лятна-игралница" class="navlink">Лятна Игралница</a>
         <a href="/лятна-занималница" class="navlink">Лятна Занималница</a>
+        <a href="/за-нас" class="navlink">За Нас</a>
+        <a href="/контакти" class="navlink">Контакти</a>
       </nav>`
 
-    const hamburger = 
-    `
-    <svg xmlns="http://www.w3.org/2000/svg" alt="Hamburger menu button" id="hamburgerIcon" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>`
-    
     function onMobile() {
       let check = false;
       (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
