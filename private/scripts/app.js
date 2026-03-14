79e676f93624d4770214307193fe0e8669abc8b0
Numpunk <99312999+Numpunk@users.noreply.github.com>
2026-03-14 13:11:10 +0200

    Update app.js

--- private/scripts/app.js
+++ private/scripts/app.js
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', function() {
   const darkgreen = '#84387b'
   var slides,dots,slideShowTimeout,slideShowTimeout;
 
-
   function hexToRgb(hex) {
     // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
     var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
