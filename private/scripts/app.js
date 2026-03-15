522ce68e3403b44d4319fa2f790adaf0e5c6cbe0
Numpunk <99312999+Numpunk@users.noreply.github.com>
2026-03-15 18:41:28 +0200

    Moved changes to private folder

--- private/scripts/app.js
+++ private/scripts/app.js
@@ -216,7 +216,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
         // HOME / SUMMER-IGR / SCHOOL-IGR --START--
-        if ((path == '/') | (path == '/училищна-занималня') | (path == '/лятна-занималня') | (path == '/допълнителни-дейности')) {
+        if ((path == '/') | (path == '/училищна-занималня') | (path == '/лятна-занималня')) {
             var slideIndex = 0;
             var slideShowTimeoutMiliseconds = 7000;
             function initSlideshow() {
@@ -341,7 +341,7 @@ document.addEventListener('DOMContentLoaded', function () {
         !function (f, b, e, v, n, t, s) {
             if (f.fbq) return; n = f.fbq = function () {
                 n.callMethod ?
-                n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
             };
             if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
             n.queue = []; t = b.createElement(e); t.async = !0;
